75defdeb7b28a2de4dbf68be6d1fe9d22933397c
Elad0109 <elad0109@gmail.com>
2025-11-06 15:08:45 +0200

    Fix Java 21 compatibility issues - Replace javax.annotation with jakarta.annotation and update tests to JUnit 5

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,6 @@
 import com.example.App;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
